1897573867c682d8f7c5ec2cac36b36ead12f56c
Kiattisak Dujda <silverfin@Kiattisaks-MBP.home>
2018-05-29 17:34:54 +0100

    Added pocket.io

--- server/server.js
+++ server/server.js
@@ -1,17 +1,28 @@
 const path = require('path');
-const publicPath = path.join(__dirname, '../public');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname, '../public');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.get('/', (req, res) => {
-    res.send('It worked!!!')
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    });
 });
 
-app.listen(PORT, () => {
+
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}...`);
 });
